2b27f1a269fa0d25a68e691ef7772fdd6d2954a4
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-14 22:37:40 +0000

    Feat: fontend finalized

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
+import AuthProvider from './context/authContext.jsx';
+import PredictionProvider from './context/PredictionContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <PredictionProvider>
+          <App />
+        </PredictionProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
