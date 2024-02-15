3639237f2e47d6dec65a8c5a4b353326c208473b
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-15 21:54:26 +0000

    Feat: login

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/config";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [signMsg, setSignMsg] = useState(null);
   const [user, setUser] = useState(() => {
     const storedUser = sessionStorage.getItem('user');
     return storedUser ? JSON.parse(storedUser) : null;
@@ -16,33 +18,43 @@ const AuthProvider = ({ children }) => {
     sessionStorage.setItem('user', JSON.stringify(user));
   }, [user]);
 
-  const login = (userData) => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    if (storedUser && userData.password === storedUser.password && userData.username === storedUser.username && userData.email === storedUser.email) {
-      setUser(userData);
-      setIsLogin(true)
-      navigate('/')
-      localStorage.setItem("isLogin", JSON.stringify(isLogin));
-    } else if (userData.username !== storedUser.username) {
-      alert("Invalid username. Please try again.");
-    } else if (userData.email !== storedUser.email) {
-      alert("Invalid email. Please try again.");
-    } else if (userData.password !== storedUser.password) {
-      alert("Invalid password. Please try again.");
-    } else {
-      alert("All fields are required.");
+  // const login = (userData) => {
+  //   const storedUser = JSON.parse(localStorage.getItem("user"));
+  //   if (storedUser && userData.password === storedUser.password && userData.username === storedUser.username && userData.email === storedUser.email) {
+  //     setUser(userData);
+  //     setIsLogin(true)
+  //     navigate('/')
+  //     localStorage.setItem("isLogin", JSON.stringify(isLogin));
+  //   } else if (userData.username !== storedUser.username) {
+  //     alert("Invalid username. Please try again.");
+  //   } else if (userData.email !== storedUser.email) {
+  //     alert("Invalid email. Please try again.");
+  //   } else if (userData.password !== storedUser.password) {
+  //     alert("Invalid password. Please try again.");
+  //   } else {
+  //     alert("All fields are required.");
 
-    }
+  //   }
+  // };
+  const login = (userData) => {
+    axios.post(`${BASE_URL}api/auth/login/`, userData).then(response => {
+      console.log(response)
+    }).catch(err => {
+      console.error(err)
+    })
+    
   };
 
   // const signup = (userData) => {
   //   localStorage.setItem("user", JSON.stringify(userData));
   //   setUser(userData);
   // };
+
   const signup = (userData) => {
-    axios.post('https://cointossapi.pythonanywhere.com/api/auth/register/', userData)
+    axios.post(`${BASE_URL}api/auth/register/`, userData)
       .then(response => {
-        console.log(response)
+        console.log(response.data)
+        setSignMsg(response.data)
       }).catch(err => {
         console.error(err)
       })
